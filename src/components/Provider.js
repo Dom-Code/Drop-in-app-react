17c8323f21732ef26fb93ccbc6d2c3551e3ea313
Dom-Code <59179076+Dom-Code@users.noreply.github.com>
2022-08-12 14:32:03 -0500

    provider cards reformatted

--- src/components/Provider.js
+++ src/components/Provider.js
@@ -47,8 +47,8 @@ const Provider = ({click}) => {
           <h3>{`${currentProvider.first_name} ${currentProvider.last_name} MD`}</h3>
           <img alt="provider" src={doc} />
           <div id='provider_data' className='tl pa4'>
-            <p>Location: {currentProvider.city}</p>
             <p>Specialty: {currentProvider.specialty}</p>
+            <p>Location: {currentProvider.city}</p>
             <p><a className='display-links' title="Login" href="/" onClick={click} >Sign in</a> or <a className="display-links" id="create" href="/" title="CreateUser" onClick={click}>Create an Account</a> to contact this provider</p>
           </div>
         </div>
